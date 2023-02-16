2f3dc779d0960297e4e3f36b4a29ce8909f2b7f9
Ri4yy <90632453+Ri4yy@users.noreply.github.com>
2023-02-16 10:53:18 +0000

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
     content: ['./Pages/**/*.{html,js}','./index.html'],
     theme: {
         extend: {
-            
+            fontFamily: {
+                'open-sans': ['Open Sans', 'sans-serif'],
+                'tinos': ['Tinos', 'serif'],
+              }
         },
 },
 plugins: [],
